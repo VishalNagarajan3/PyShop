d389beca12dc7547cddfb1bec4a96e45742177e1
VishalNagarajan3 <vishalnagarajan6@gmail.com>
2024-10-18 23:34:34 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-# Create your models here.
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
@@ -17,8 +17,7 @@ class Offer(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    # Add other fields as necessary
-
+  
 class Cart(models.Model):
     total = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
